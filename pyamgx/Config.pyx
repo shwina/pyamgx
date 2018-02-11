75cc54842f4de10356a215bc440e5a30bdcb5932
Ashwin Srinath <atrikut@clemson.edu>
2018-02-11 07:43:53 -0500

    Add documentation for Config class

--- pyamgx/Config.pyx
+++ pyamgx/Config.pyx
@@ -1,16 +1,39 @@
 
 cdef class Config:
-
+    """
+    Config: Class for storing parsed representations of parameter strings.
+    """
     cdef AMGX_config_handle cfg
     cdef public AMGX_RC _err
 
     def create(self, options):
+        """
+        cfg.create(options)
+
+        Create configuration from options string
+
+        Parameters:
+
+        options: str or bytes_like
+            Options string
+        """
         if not isinstance(options, bytes):
             options = options.encode()
         self._err = AMGX_config_create(&self.cfg, options)
         return self
 
     def create_from_file(self, param_file):
+        """
+        cfg.create_from_file(param_file)
+
+        Create configuration from config file
+
+        Parameters:
+        ----------
+
+        param_file: str or bytes_like
+            Path to configuration file
+        """
         if not isinstance(param_file, bytes):
             param_file = param_file.encode()
         self._err = AMGX_config_create_from_file(&self.cfg, param_file)
