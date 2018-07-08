6105b0159176cca186a4ad1098990cfdcf05ec27
Ashwin Srinath <atrikut@clemson.edu>
2018-07-08 10:49:23 -0400

    Add option to use AMGX_BUILD_DIR in setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@ import subprocess
 import numpy
 
 try:
-    amgx_dir = os.environ['AMGX_DIR']
+    AMGX_DIR = os.environ['AMGX_DIR']
 except KeyError:
     raise EnvironmentError("AMGX_DIR environment variable not set."
             "Set AMGX_DIR to the "
             "root directory of your AMGX installation.")
+
+try:
+    AMGX_BUILD_DIR = os.environ['AMGX_BUILD_DIR']
+except KeyError:
+    AMGX_BUILD_DIR = os.path.join(AMGX_DIR, 'build')
 
 from Cython.Build import cythonize
 ext = cythonize([
@@ -19,10 +24,20 @@ ext = cythonize([
         depends=['pyamgx/*.pyx, pyamgx/*.pxi'],
         libraries=['amgxsh'],
         language='c',
-        include_dirs = [numpy.get_include(), amgx_dir+'/base/include', amgx_dir+'/core/include'],
-        library_dirs = [numpy.get_include(), amgx_dir+'/build'],
-        runtime_library_dirs = [numpy.get_include(), amgx_dir+'/build'],
-    )])
+        include_dirs = [
+            numpy.get_include(),
+            os.path.join(AMGX_DIR, 'base/include'),
+            os.path.join(AMGX_DIR, 'core/include')
+        ],
+        library_dirs = [
+            numpy.get_include(),
+            AMGX_BUILD_DIR
+        ],
+        runtime_library_dirs = [
+            numpy.get_include(),
+            AMGX_BUILD_DIR
+        ],
+)])
 
 setup(name='pyamgx',
       author='Ashwin Srinath',
