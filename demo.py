d3c0437b588d0abde5185b12f9e76cf50c8625b9
Ashwin Srinath <shwina@users.noreply.github.com>
2018-08-28 07:45:12 -0400

    Remove unused import os in demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
+import numpy as np
+import scipy.sparse as sparse
+import scipy.sparse.linalg as splinalg
+
 import pyamgx
-import os
 
 pyamgx.initialize()
 
@@ -29,10 +32,6 @@ x = pyamgx.Vector().create(rsc)
 solver = pyamgx.Solver().create(rsc, cfg)
 
 # Upload system:
-import numpy as np
-import scipy.sparse as sparse
-import scipy.sparse.linalg as splinalg
-
 M = sparse.csr_matrix(np.random.rand(5, 5))
 rhs = np.random.rand(5)
 sol = np.zeros(5, dtype=np.float64)
