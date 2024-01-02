fcc06a4420feaecf2f26ca4d7b108e34b5e79ebc
yanang007 <yanang007@users.noreply.github.com>
2024-01-03 01:02:15 +0800

    Remove packaging support
    Fix a potential NameError of lib_path

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from distutils.sysconfig import get_python_lib
 import  os
 from os.path import join as pjoin
 from setuptools import setup, Extension
@@ -19,7 +18,8 @@ if not AMGX_DIR:
     # look in PREFIX:
     PREFIX = sys.prefix
     if os.path.isfile(os.path.join(PREFIX, f'lib/{lib_name}')):
-        AMGX_lib_dirs = [os.path.join(PREFIX, 'lib')]
+        lib_path = os.path.join(PREFIX, 'lib')
+        AMGX_lib_dirs = [lib_path]
         AMGX_include_dirs = [os.path.join(PREFIX, 'include')]
     else:
         raise EnvironmentError(f'AMGX_DIR not set and {lib_name} not found')
@@ -45,17 +45,11 @@ lib_file_path = os.path.join(lib_path, lib_name)
 runtime_lib_dirs = []
 data_files = []
 
-if 'install' in sys.argv[1:]:
-    if sys.platform == "win32":
+if sys.platform == "win32":
+    if 'install' in sys.argv[1:]:
         data_files = [('', [lib_file_path])]
-    else:
-        runtime_lib_dirs = [numpy.get_include(),] + AMGX_lib_dirs
 else:
-    # bdist_wheel or other subcommand
-    data_files = [(
-        os.path.relpath(get_python_lib(), sys.prefix), # should put the runtime under "lib\site-packages"
-        [lib_file_path]
-    )]  # add the runtime library into package
+    runtime_lib_dirs = [numpy.get_include(), ] + AMGX_lib_dirs
 
 from Cython.Build import cythonize
 ext = cythonize([
